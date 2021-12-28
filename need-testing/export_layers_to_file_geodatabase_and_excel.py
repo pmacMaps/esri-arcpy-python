6c8dd41f835ce4791f21c3dac3538117c6b0da4c
Patrick McKinney <pnmcartography@gmail.com>
2021-12-28 06:49:47 -0500

    stage export layers to file gdb and excel script for further testing

--- need-testing/export_layers_to_file_geodatabase_and_excel.py
+++ need-testing/export_layers_to_file_geodatabase_and_excel.py
@@ -1,4 +1,4 @@
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Name:        Export Layers to File Geodatabase and Microsoft Excel
 #
 # Purpose:     Creates a new directory using the current date.  A file geodatabase
@@ -7,70 +7,69 @@
 #              After field calculations, the layers in the file geodatabase are exported
 #              to Microsoft Excel format in the newly created directory.
 #
-# Author:      Patrick McKinney, Cumberland County, PA
+# Author:      Patrick McKinney
 #
 # Created:     4/4/2019
 #
-# Updated:     4/18/2019
-#
-# Disclaimer: CUMBERLAND COUNTY ASSUMES NO LIABILITY ARISING FROM USE OF THESE MAPS OR DATA. THE MAPS AND DATA ARE PROVIDED WITHOUT
-# WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-# FITNESS FOR A PARTICULAR PURPOSE.
-# Furthermore, Cumberland County assumes no liability for any errors, omissions, or inaccuracies in the information provided regardless
-# of the cause of such, or for any decision made, action taken, or action not taken by the user in reliance upon any maps or data provided
-# herein. The user assumes the risk that the information may not be accurate.
-#-------------------------------------------------------------------------------
+# Updated:     12/28/2021
+# -------------------------------------------------------------------------------
 
-import arcpy, datetime, sys, os
+import arcpy
+import datetime
+import sys
+import os
 
 try:
     # Time stamp variables
-    currentTime = datetime.datetime.now()
+    date_today = datetime.date.today()
     # Date formatted as month-day-year (1-1-2017)
-    dateToday = currentTime.strftime("%m-%d-%Y")
+    formatted_date_today = date_today.strftime("%m-%d-%Y")
     # date for file geodatabase name
-    dateGdb = currentTime.strftime("%m%d%Y")
+    date_gdb = date_today.strftime("%m%d%Y")
 
     # Create text file for logging results of script
     # update this variable
-    log_file = r'[path]\[to]\[location]\Report File Name {}.txt'.format(dateToday)
+    log_file = r'[path]\[to]\[location]\Report File Name {}.txt'.format(
+        formatted_date_today)
     # variable to store messages for log file. Messages written in finally statement at end of script
-    logMsg = ''
+    log_message = ''
 
     # sde database connection
     # update this variable
     sde = r'Database Connections\[SDE Database Name].sde'
     # layers
     # update this variable
-    layers = [[os.path.join(sde, r'Name of Layer'), 'Name of Layer'], [os.path.join(sde, r'Name of Layer'), 'Name of Layer'], [os.path.join(sde, r'Name of Layer'), 'Name of Layer']]
+    layers = [[os.path.join(sde, r'Name of Layer'), 'Name of Layer'], [os.path.join(
+        sde, r'Name of Layer'), 'Name of Layer'], [os.path.join(sde, r'Name of Layer'), 'Name of Layer']]
 
     # 1. create new directory
     # parent directory
     # update this variable
     parent_dir = r'[path]\[to]\[location]'
     # output directory
-    out_dir = r'{}\{}'.format(parent_dir,dateGdb)
+    out_dir = r'{}\{}'.format(parent_dir, date_gdb)
     # create sub-directory with current date
     os.mkdir(out_dir)
     # add message
-    logMsg += '\nCreated directory "{}" in {}\n'.format(dateGdb,out_dir)
+    log_message += '\nCreated directory "{}" in {}\n'.format(date_gdb, out_dir)
 
     # 2. create file geodatabase
     # parameters
     # update this variable
-    out_gdb_name = r'Name_of_Geodatabase_{}'.format(dateGdb)
+    out_gdb_name = r'Name_of_Geodatabase_{}'.format(date_gdb)
     out_gdb = os.path.join(out_dir, '{}.gdb'.format(out_gdb_name))
     # geoprocessing
-    arcpy.CreateFileGDB_management(out_dir,out_gdb_name,'10.0')
+    arcpy.CreateFileGDB_management(out_dir, out_gdb_name, '10.0')
     # add message
-    logMsg += '\nCreated file geodatabase "{}" in directory "{}"\n'.format(out_gdb_name, out_dir)
+    log_message += '\nCreated file geodatabase "{}" in directory "{}"\n'.format(
+        out_gdb_name, out_dir)
 
     # 3. Export layers to file geodatabase
     for fc in layers:
         # export feature class
-        arcpy.FeatureClassToFeatureClass_conversion(fc[0],out_gdb,fc[1])
+        arcpy.FeatureClassToFeatureClass_conversion(fc[0], out_gdb, fc[1])
         # add message
-        logMsg += '\nCopied {} layer to {}\n'.format(fc[1],out_gdb)
+        log_message += '\nCopied {} layer to {}\n'.format(fc[1], out_gdb)
     # end for
 
     # 4. update Latitude Longitude fields in feature classes
@@ -96,29 +95,23 @@ try:
                 cursor.updateRow(row)
             # end for
             # update your message
-            logMsg += '\nCompleted updating Latitude and Longitude records for "{}" layer\n'.format(fc)
+            log_message += '\nCompleted updating Latitude and Longitude records for "{}" layer\n'.format(
+                fc)
         # end cursor
         # convert to Excel
-        arcpy.TableToExcel_conversion(fc,os.path.join(out_dir,'{}.xls'.format(fc)),"ALIAS")
+        arcpy.TableToExcel_conversion(fc, os.path.join(
+            out_dir, '{}.xls'.format(fc)), "ALIAS")
         # add message
-        logMsg += '\nExported {} layer to Microsof Excel format\n'.format(fc)
+        log_message += '\nExported {} layer to Microsof Excel format\n'.format(
+            fc)
     # end for
 # If an error occurs running geoprocessing tool(s) capture error and write message
-# handle error outside of Python system
-except EnvironmentError as e:
+except (Exception, EnvironmentError) as e:
     tbE = sys.exc_info()[2]
     # add the line number the error occured to the log message
-    logMsg += "\nFailed at Line {}\n".format(tbE.tb_lineno)
+    log_message += "\nFailed at Line {}\n".format(tbE.tb_lineno)
     # add the error message to the log message
-    logMsg += "\nError: {}\n".format(str(e))
-# handle exception error
-except Exception as e:
-    # Store information about the error
-    tbE = sys.exc_info()[2]
-    # add the line number the error occured to the log message
-    logMsg += "\nFailed at Line {}\n".format(tbE.tb_lineno)
-    # add the error message to the log message
-    logMsg += "\nError: {}\n".format(e.message)
+    log_message += "\nError: {}\n".format(str(e))
 finally:
     try:
         if cursor:
@@ -128,6 +121,6 @@ finally:
     # write message to log file
     try:
         with open(log_file, 'w') as f:
-            f.write(str(logMsg))
+            f.write(str(log_message))
     except:
         pass
