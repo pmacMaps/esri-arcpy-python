c7c5444149bb9d6bcd6f039c86a045e1275c876d
Patrick McKinney <pnmcartography@gmail.com>
2022-09-21 16:15:49 -0400

    removed missing variable; update module imports; use formatted string literals

--- overwrite_service_to_agol.py
+++ overwrite_service_to_agol.py
@@ -16,17 +16,18 @@
 #
 # Created:     9/11/2020
 #
-# Updated:     12/23/2021
+# Updated:     9/21/2022
 #
 # Author:      Patrick McKinney
 # --------------------------------------------------------------------------------------------------------------------------------------------
 
 # import modules
 import arcpy
-import os
 import sys
-import datetime
 import time
+from datetime import date
+from os import path
+from os import environ
 from arcgis.gis import GIS
 
 # attempt to run code; if an error occurs, error messages will be logged in a text file
@@ -35,15 +36,14 @@ try:
     start_time = time.perf_counter()
 
     # Date the script is being run
-    date_today = datetime.date.today()
+    date_today = date.today()
     # Date formatted as month-day-year (1-1-2020)
     formatted_date_today = date_today.strftime("%m-%d-%Y")
 
     # variable to store messages for log file. Messages written in finally statement at end of script
     log_message = ''
     # Create text file for logging messages of script progress and/or errors
-    log_file = r'Path\To\Directory\Report File {}.txt'.format(
-        formatted_date_today)
+    log_file = path.join(r'Path\To\Directory', f'Report File {formatted_date_today}.txt')
 
     # Set the path to the ArcGIS Pro project
     # this project contains the local dataset the feature service is being updated for
@@ -57,10 +57,10 @@ try:
     portal = ""
     # user name of owner of item (admin users may be able to overwrite)
     # create environment variable to store username; pass that variable name into get() method
-    user = os.environ.get('user_name_environment_variable')
+    user = environ.get('user_name_environment_variable')
     # password of owner of item (admin users may be able to overwrite)
     # create environment variable to store pasword; pass that variable name into get() method
-    password = os.environ.get('password_environment_variable')
+    password = environ.get('password_environment_variable')
 
     # sign-in to ArcGIS Online or Portal within ArcGIS Pro project
     arcpy.SignInToPortal(portal, user, password)
@@ -74,13 +74,13 @@ try:
     shrGroups = ""  # name of group(s), i.e, 'Public Safety'
 
     # Local paths to create temporary content
-    relPath = os.path.dirname(projPath)
+    relPath = path.dirname(projPath)
     # service definition draft file
-    sddraft = os.path.join(relPath, "WebUpdate.sddraft")
+    sddraft = path.join(relPath, "WebUpdate.sddraft")
     # service defition
     # this is what is being overwritten to ArcGIS Online
     # this is how you push your new data to ArcGIS Online from the local source
-    sd = os.path.join(relPath, "WebUpdate.sd")
+    sd = path.join(relPath, "WebUpdate.sd")
 
     # Create a new SDDraft and stage to SD
     log_message += "Creating Service Defintion file\n"
@@ -102,7 +102,7 @@ try:
     arcpy.StageService_server(sddraft, sd)
 
     # add message
-    log_message += "\nConnecting to {}\n".format(portal)
+    log_message += f"\nConnecting to {portal}\n"
     # Connect to ArcGIS Online or Portal
     # may need to add 'verify_cert=False' argument at end of function call
     gis = GIS(portal, user, password)
@@ -114,14 +114,14 @@ try:
     sdItem = gis.content.get(sd_id)
 
     # add message
-    log_message += "\nFound SD: {}, ID: {}\n".format(sdItem.title, sdItem.id)
+    log_message += f"\nFound SD: {sdItem.title}, ID: {sdItem.id}\n"
     # update data for service definition ArcGIS Online/Portal item using the service definition file created in ArcGIS Pro
     sdItem.update(data=sd)
 
     # add message
     log_message += "\nOverwriting existing feature service\n"
     # overwrite feature service so it will now reference updated data pushed from a local source
-    fs = sdItem.publish(publish_parameters=pub_params, overwrite=True)
+    fs = sdItem.publish(overwrite=True)
 
     # set sharing options for feature service
     if shrOrg or shrEveryone or shrGroups:
@@ -139,15 +139,14 @@ try:
     elapsed_time_minutes = round((elapsed_time / 60), 2)
 
     # add message
-    log_message += "\nOverwrote 'Some Dataset' feature service to ArcGIS Online in {}-minutes on {}\n".format(
-        elapsed_time_minutes, formatted_date_today)
+    log_message += f"\nOverwrote 'Some Dataset' feature service to ArcGIS Online in {elapsed_time_minutes}-minutes on {formatted_date_today}\n"
 # If an error occurs running geoprocessing tool(s) capture error and write message
 except (Exception, EnvironmentError) as e:
     tbE = sys.exc_info()[2]
     # Write the line number the error occured to the log file
-    log_message += "\nFailed at Line {}\n".format(tbE.tb_lineno)
+    log_message += f"\nFailed at Line {tbE.tb_lineno}\n"
     # Write the error message to the log file
-    log_message += "Error: {}".format(str(e))
+    log_message += f"Error: {str(e)}"
 finally:
     # write message to log file
     try:
